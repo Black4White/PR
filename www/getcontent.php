f1c4225824352e8af30d63b6e15c92cfa4496de6
Dmitrii <47597564+Black4White@users.noreply.github.com>
2020-05-27 16:50:49 +0300

    Delete getcontent.php

--- www/getcontent.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$offset = (!isset($_GET['offset']) || !$_GET['offset']) ? 0: $_GET['offset'];
-$filename = "chat.txt";
-$content = file_get_contents($filename, FALSE, NULL, $offset);
-echo $content;
-?>
